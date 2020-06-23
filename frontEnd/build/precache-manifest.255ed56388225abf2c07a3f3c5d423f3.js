c3670d357a8c899dd4aeccdd9cf850e85f34911c
Krizzia Mae Fortich <krizziafortich@gmail.com>
2020-06-23 21:32:18 +0800

    frontend build

---
+++ frontEnd/build/precache-manifest.255ed56388225abf2c07a3f3c5d423f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6600fa06104d65b52aba1dad87f1c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff7fec4d612c07dd997",
+    "url": "/static/css/2.6a21e517.chunk.css"
+  },
+  {
+    "revision": "51a85bdf715d1a3446a0",
+    "url": "/static/css/main.989c83fd.chunk.css"
+  },
+  {
+    "revision": "7ff7fec4d612c07dd997",
+    "url": "/static/js/2.b8544478.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.b8544478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a85bdf715d1a3446a0",
+    "url": "/static/js/main.815296e0.chunk.js"
+  },
+  {
+    "revision": "bd8de3808937d6b8670d",
+    "url": "/static/js/runtime-main.e8430fdd.js"
+  }
+]);
