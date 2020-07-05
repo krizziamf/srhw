0c449213adc2f70d8290f63048300b6b5fbc9ac0
Krizzia Mae Fortich <krizziafortich@gmail.com>
2020-07-05 17:23:42 +0800

    changed be

--- backEnd/src/config/email.config.js
+++ backEnd/src/config/email.config.js
@@ -20,18 +20,3 @@ module.exports = nodeMailer.createTransport({
 });
 
 //config ng transport ni nodemailer. confgi ng email
-
-// module.exports = nodeMailer.createTransport({
-//   host: process.env.EMAIL_HOST,
-//   port: process.env.EMAIL_PORT,
-//   secure: process.env.EMAIL_SECURE,
-//   auth: {
-//     type: process.env.EMAIL_AUTH_TYPE,
-//     clientId: process.env.EMAIL_AUTH_CLIENT_ID,
-//     clientSecret: process.env.EMAIL_AUTH_CLIENT_SECRET,
-//     user: process.env.EMAIL_AUTH_USER,
-//     refreshToken: process.env.EMAIL_AUTH_REFRESH_TOKEN,
-//     accessToken: process.env.EMAIL_AUTH_ACCESS_TOKEN,
-//     expires: process.env.EMAIL_AUTH_EXPIRES
-//   }
-// });
